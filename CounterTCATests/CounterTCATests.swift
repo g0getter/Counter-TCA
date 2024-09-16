b10f2520768e16d947e741e54a0f1ca6a453f7ec
나경 <nagyeong.yeo1023@gmail.com>
2024-09-16 16:52:16 +0900

    Essentials/Testing your feature: Section1, 2
    Section1. Testing state changes
    Section2. Testing effects

---
+++ CounterTCATests/CounterTCATests.swift
@@ -0,0 +1,34 @@
+//
+//  CounterTCATests.swift
+//  CounterTCATests
+//
+//  Created by 여나경 on 9/16/24.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import CounterTCA
+
+final class CounterFeatureTests: XCTestCase {
+    func testTimer() async {
+        let clock = TestClock()
+        
+        let store = await TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        } withDependencies: {
+          $0.continuousClock = clock
+        }
+        
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = true
+        }
+        await clock.advance(by: .seconds(1))
+        await store.receive(\.timerTick) {
+            $0.count = 1
+        }
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = false
+        }
+    }
+}
